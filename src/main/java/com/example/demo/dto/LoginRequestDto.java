6bbaef96054ded9b315a70bf33fd7cf83f66c7fe
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-03-07 21:47:02 +0900

    BoardAndReplyTable

--- src/main/java/com/example/demo/dto/LoginRequestDto.java
+++ src/main/java/com/example/demo/dto/LoginRequestDto.java
@@ -1,9 +1,13 @@
 package com.example.demo.dto;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
+
+import javax.validation.constraints.Size;
 
 @Data
 public class LoginRequestDto {
     private String userName;
+
     private String password;
 }
---
+++ src/main/java/com/example/demo/model/Reply.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    //한 게시물에는 여러개의 답글이 있을 수 있다.
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    //한 유저는 여러개의 답글을 달 수 있다.
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
