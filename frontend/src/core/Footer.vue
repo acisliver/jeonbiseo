a04e96045deaf51d086d7bcd6c3f21957714558c
acisliver <7273322@naver.com>
2021-03-09 12:06:13 +0900

    chunk 에러 수정

--- frontend/src/core/Footer.vue
+++ frontend/src/core/Footer.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="footer">
-    <h1>This is a footer</h1>
-  </div>
+  <v-footer
+      color= "#42b983"
+      padless
+  >
+    <v-row
+        justify="center"
+        no-gutters
+    >
+      <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+      >
+        <v-icon size="24px">
+          {{ icon }}
+        </v-icon>
+      </v-btn>
+
+      <v-col
+          class="#42b983 py-4 text-center white--text"
+          cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>전자기기 비교는 여기서</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
+  data: () => ({
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
 }
 </script>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import store from '@/store'
+import Login from "@/components/Login";
 
 Vue.use(VueRouter)
 
 const Home = () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
 const About = () => import(/* webpackChunkName: "about" */ '../views/About.vue');
-const Login = () => import(/* webpackChunkName: "login" */ '../components/Login.vue');
+// const Login = () => import(/* webpackChunkName: "login" */ '../components/Login.vue');
 const MyPage = () => import(/* webpackChunkName: "mypage" */ '../views/MyPage.vue');
 const SignUp = () => import(/* webpackChunkName: "signup" */ '../components/SignUp.vue');
 const Board = () => import(/* webpackChunkName: "board" */ '../views/Board.vue');
