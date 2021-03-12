7aac50557e7e782e6f959d7b939282db440aee2f
acisliver <7273322@naver.com>
2021-03-12 15:38:32 +0900

    자유게시판 게시글 작성 생성

--- frontend/src/components/WriteFreeBoard.vue
+++ frontend/src/components/WriteFreeBoard.vue
@@ -1,10 +1,46 @@
 <template>
-  <div class="writeBoard">자유게시판글쓰기</div>
+  <div class="writeBoard container">
+    <h2>자유게시판글쓰기</h2>
+    <v-text-field
+        v-model="contents.title"
+        label="제목"
+    ></v-text-field>
+    <v-textarea
+        v-model="contents.content"
+        filled
+        rows="4"
+        height="600"
+    ></v-textarea>
+    <v-btn @click="saveContent(contents)">저장</v-btn>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "WriteBoard"
+  name: "WriteBoard",
+  data(){
+    return {
+      contents:{
+        title:'',
+        content:''
+      }
+    }
+  },
+  methods: {
+    saveContent(contentObj){
+      console.log(contentObj)
+      axios
+      .post('/api/free-board/write/save', contentObj)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
