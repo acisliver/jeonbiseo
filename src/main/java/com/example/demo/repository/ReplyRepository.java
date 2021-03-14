63887dc1d7f5bf4a00fdceb90a8ac12d2d1bbaee
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2021-03-14 11:35:06 +0900

    오타 수정

--- src/main/java/com/example/demo/repository/ReplyRepository.java
+++ src/main/java/com/example/demo/repository/ReplyRepository.java
@@ -10,7 +10,7 @@ public interface ReplyRepository extends JpaRepository<Reply,Integer> {
 
     //insert를 할 것이기 때문에 @Modifying 필요, @Modifying는 int만 반활 할 수 있다.
     @Modifying
-    @Query(value = "INSERT INTO reply(userID, boardId, content, createDate,reparent, redepth, reoder) VALUE(?1,?2,?3,now(),?4,?5,?6)",nativeQuery = true)
+    @Query(value = "INSERT INTO reply(userID, boardId, content, createDate,reparent, redepth, reorder) VALUE(?1,?2,?3,now(),?4,?5,?6)",nativeQuery = true)
     int replySave(int userId, int boardId, String content, int reparent, int redepth, int reoder);
 
     @Query(value = "Update  reply Set reorder=reorder+1 WHERE reorder>=?1", nativeQuery = true)
