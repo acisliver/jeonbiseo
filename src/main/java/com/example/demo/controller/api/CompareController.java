5a2ac0f4bf3f041d2b91adeb1dce2ea70ade5d8a
hunko <rhwlgns4386@naver.com>
2021-05-27 17:00:17 +0900

    select get방식으로 변경

--- src/main/java/com/example/demo/controller/api/CompareController.java
+++ src/main/java/com/example/demo/controller/api/CompareController.java
@@ -5,10 +5,7 @@ import com.example.demo.model.Application;
 import com.example.demo.model.ESApplication;
 import com.example.demo.service.CompareService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +33,7 @@ public class CompareController {
     }
 
     @GetMapping("/api/compare/select")
-    public Optional<Application> selectApplication(@RequestBody SearchTermDto searchTermDto){
-        return compareService.findApplication(searchTermDto.getSearchTerm());
+    public Optional<Application> selectApplication(@RequestParam String sqlId){
+        return compareService.findApplication(sqlId);
     }
 }
