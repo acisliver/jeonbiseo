a63bd4ce43532bf248b8b54d0d6856f3523a9119
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-05-29 22:02:19 +0900

    DTO생성

---
+++ src/main/java/com/example/demo/dto/RequestUserPreferenceDto.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class RequestUserPreferenceDto {
+    private String os;
+    private String weight;
+    private String size;
+    private String usePen;
+    private String network;
+}
