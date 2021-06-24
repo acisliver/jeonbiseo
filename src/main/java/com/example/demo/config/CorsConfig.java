0225254a8aea00c655a49d4064b49b18d24feb35
acisliver <7273322@naver.com>
2021-06-24 19:56:26 +0900

    서버측 cors 허용

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); //내 서버가 응답을 할 때 json을 js에서 처리 할수 있게 하겠다.
-        config.addAllowedOrigin("*"); //모든 ip에 응답을 하겠다.
+        config.addAllowedOriginPattern(CorsConfiguration.ALL); //모든 ip에 응답을 하겠다.
         config.addAllowedHeader("*"); //모든 header에 응답을 하겠다.
         config.addAllowedMethod("*"); //모든 post,get,put,delete,patch요청을 허용
         source.registerCorsConfiguration("/api/**",config);
